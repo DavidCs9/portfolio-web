916fa1dcd3d86d75359054849ac8a88f2abc8bc0
DavidCs9 <davidcastrosi@hotmail.com>
2023-02-15 12:23:05 -0600

    add transition for work

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-        }
-})
-})
-
-
-const hiddenElements = document.querySelectorAll('.container')
-console.log(hiddenElements)
-hiddenElements.forEach((el) => observer.observe(el))
-
-
-console.log(document.querySelectorAll('.hidden'))
+const container = document.querySelectorAll(".container");
+const wk = document.querySelectorAll(".wk");
+container.forEach((el) => observer.observe(el));
+wk.forEach((el) => observer.observe(el));
