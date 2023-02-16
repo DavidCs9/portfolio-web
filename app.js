efc81457cc51d8698863452ec44486495d6db4cc
DavidCs9 <davidcastrosi@hotmail.com>
2023-02-15 21:10:56 -0600

    change tindog project of place

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+      entry.target.classList.add('show')
     } else {
-      entry.target.classList.remove("show");
+      entry.target.classList.remove('show')
     }
-  });
-});
+  })
+})
 
-const container = document.querySelectorAll(".container");
-const wk = document.querySelectorAll(".wk");
-container.forEach((el) => observer.observe(el));
-wk.forEach((el) => observer.observe(el));
+const container = document.querySelectorAll('.container')
+const wk = document.querySelectorAll('.wk')
+container.forEach((el) => observer.observe(el))
+wk.forEach((el) => observer.observe(el))
